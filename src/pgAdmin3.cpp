3fea415d76ba3678f08d91abee11a86a4c99d0e0
Mark Yeatman <myeatman@vale-housing.co.uk>
2002-10-04 17:07:40 +0000

    Added IfDef on the 3d controls line (Only works in Windows)
    git-svn-id: svn://svn.pgadmin.org/trunk@749 a7884b65-44f6-0310-8a51-81a127f17b15

--- src/pgAdmin3.cpp
+++ src/pgAdmin3.cpp
@@ -29,18 +29,20 @@ IMPLEMENT_APP(pgAdmin3)
 // The Application!
 bool pgAdmin3::OnInit()
 {
-    
+
     // Setup logging first
     objLogger = new sysLogger();
     wxLog::SetActiveTarget(objLogger);
-    
+
     // Show the splash screen
     frmSplash* winSplash = new frmSplash((wxFrame *)NULL);
     SetTopWindow(winSplash);
     winSplash->Show(TRUE);
-    
+
     // Set some defaults
+#ifdef _WXMSW_
     SetAuto3D(TRUE);
+#endif
     SetAppName(APPNAME);
 
     // Load the Settings
@@ -49,15 +51,15 @@ bool pgAdmin3::OnInit()
 #ifndef _DEBUG
     wxSleep(2);
 #endif
-    
+
     // Create & show the main form
     winMain = new frmMain(wxT("pgAdmin III"), wxPoint(objSettings->GetFrmMainLeft(), objSettings->GetFrmMainTop()), wxSize(objSettings->GetFrmMainWidth(), objSettings->GetFrmMainHeight()));
     winMain->Show(TRUE);
     SetTopWindow(winMain);
     SetExitOnFrameDelete(TRUE);
-    
+
     winSplash->Close();
-    
+
     return TRUE;
 }
 
