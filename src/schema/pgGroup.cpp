9b56e11b6a1ff7dfaa5c2867368892114582c8a0
Andreas Pflug <pgadmin@pse-consulting.de>
2003-03-31 23:13:09 +0000

    created
    git-svn-id: svn://svn.pgadmin.org/trunk@925 a7884b65-44f6-0310-8a51-81a127f17b15

---
+++ src/schema/pgGroup.cpp
@@ -0,0 +1,129 @@
+//////////////////////////////////////////////////////////////////////////
+//
+// pgAdmin III - PostgreSQL Tools
+// Copyright (C) 2002, The pgAdmin Development Team
+// This software is released under the pgAdmin Public Licence
+//
+// pgGroup.cpp - Group class
+//
+//////////////////////////////////////////////////////////////////////////
+
+// wxWindows headers
+#include <wx/wx.h>
+
+// App headers
+#include "pgAdmin3.h"
+#include "misc.h"
+#include "pgObject.h"
+#include "pgGroup.h"
+#include "pgCollection.h"
+#include "frmMain.h"
+
+
+pgGroup::pgGroup(const wxString& newName)
+: pgObject(PG_GROUP, newName)
+{
+    wxLogInfo(wxT("Creating a pgGroup object"));
+    memberCount=0;
+}
+
+pgGroup::~pgGroup()
+{
+    wxLogInfo(wxT("Destroying a pgGroup object"));
+}
+
+
+wxString pgGroup::GetSql(wxTreeCtrl *browser)
+{
+    if (sql.IsNull())
+    {
+        sql = wxT("CREATE Group ") + GetQuotedIdentifier()
+            + wxT(" WITH SYSID ") + NumToStr(groupId)
+            + wxT("\n  USER ") + quotedMembers
+            +wxT(";\n");
+    }
+    return sql;
+}
+
+
+void pgGroup::ShowTreeDetail(wxTreeCtrl *browser, frmMain *form, wxListCtrl *properties, wxListCtrl *statistics, ctlSQLBox *sqlPane)
+{
+    if (!expandedKids)
+    {
+        expandedKids=true;
+
+        wxString ml=memberIds;
+        ml.Replace(" ", ",");
+        pgSet *set=server->ExecuteSet(wxT(
+            "SELECT usename FROM pg_shadow WHERE usesysid IN (") + ml + wxT(")"));
+        if (set)
+        {
+            while (!set->Eof())
+            {
+                if (memberCount)
+                {
+                    members += wxT(", ");
+                    quotedMembers += wxT(", ");
+                }
+                members += set->GetVal(0);
+                quotedMembers += qtIdent(set->GetVal(0));
+                memberCount++;
+                set->MoveNext();
+            }
+            delete set;
+        }
+    }
+
+    if (properties)
+    {
+        wxLogInfo(wxT("Displaying properties for Group ") + GetIdentifier());
+
+        properties->ClearAll();
+        properties->InsertColumn(0, wxT("Property"), wxLIST_FORMAT_LEFT, 150);
+        properties->InsertColumn(1, wxT("Value"), wxLIST_FORMAT_LEFT, 200);
+  
+
+        int pos=0;
+
+        InsertListItem(properties, pos++, wxT("Name"), GetName());
+        InsertListItem(properties, pos++, wxT("Group Id"), NumToStr(GetGroupId()));
+        InsertListItem(properties, pos++, wxT("Member Count"), NumToStr(GetMemberCount()));
+        InsertListItem(properties, pos++, wxT("Members"), GetMembers());
+    }
+}
+
+
+
+void pgGroup::ShowTreeCollection(pgCollection *collection, frmMain *form, wxTreeCtrl *browser, wxListCtrl *properties, wxListCtrl *statistics, ctlSQLBox *sqlPane)
+{
+    pgGroup *group;
+
+    if (browser->GetChildrenCount(collection->GetId(), FALSE) == 0)
+    {
+
+        wxLogInfo(wxT("Adding Groups to database"));
+
+        // Get the Groups
+        pgSet *groups= collection->GetServer()->ExecuteSet(wxT("SELECT * from pg_group"));
+
+        if (groups)
+        {
+            while (!groups->Eof())
+            {
+                group = new pgGroup(groups->GetVal(wxT("groname")));
+                group->iSetGroupId(StrToLong(groups->GetVal(wxT("grosysid"))));
+                group->iSetServer(collection->GetServer());
+                wxString mids=groups->GetVal(wxT("grolist"));
+                mids = mids.Mid(1, mids.Length()-2);
+                group->iSetMemberIds(mids);
+
+                browser->AppendItem(collection->GetId(), group->GetIdentifier(), PGICON_GROUP, -1, group);
+	    
+			    groups->MoveNext();
+            }
+
+		    delete groups;
+        }
+    }
+}
+
