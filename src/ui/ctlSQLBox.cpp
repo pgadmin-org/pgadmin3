82edd1bdfcf1f355358d27767840ca4ec39aa445
Andreas Pflug <pgadmin@pse-consulting.de>
2003-04-01 21:38:01 +0000

    Fixed font
    git-svn-id: svn://svn.pgadmin.org/trunk@936 a7884b65-44f6-0310-8a51-81a127f17b15

--- src/ui/ctlSQLBox.cpp
+++ src/ui/ctlSQLBox.cpp
@@ -23,29 +23,43 @@ ctlSQLBox::ctlSQLBox(wxWindow *parent, wxWindowID id, const wxPoint& pos, const 
     wxLogInfo(wxT("Creating a ctlSQLBox"));
 
     // Clear all styles
-    this->StyleClearAll();
+    StyleClearAll();
     
+    // Font
+    wxFont fntSQLBox(9, wxMODERN, wxNORMAL, wxNORMAL);
+    StyleSetFont(wxSTC_STYLE_DEFAULT, fntSQLBox);
+    StyleSetFont(0, fntSQLBox);
+    StyleSetFont(1, fntSQLBox);
+    StyleSetFont(2, fntSQLBox);
+    StyleSetFont(3, fntSQLBox);
+    StyleSetFont(4, fntSQLBox);
+    StyleSetFont(5, fntSQLBox);
+    StyleSetFont(6, fntSQLBox);
+    StyleSetFont(7, fntSQLBox);
+    StyleSetFont(8, fntSQLBox);
+    StyleSetFont(9, fntSQLBox);
+    StyleSetFont(10, fntSQLBox);
+    StyleSetFont(11, fntSQLBox);
+    
+
     // Setup the different highlight colurs
-    this->StyleSetForeground(0,  wxColour(0x80, 0x80, 0x80));
-    this->StyleSetForeground(1,  wxColour(0x00, 0x7f, 0x00));
-    this->StyleSetForeground(2,  wxColour(0x00, 0x7f, 0x00));
-    this->StyleSetForeground(3,  wxColour(0x7f, 0x7f, 0x7f));
-    this->StyleSetForeground(4,  wxColour(0x00, 0x7f, 0x7f));
-    this->StyleSetForeground(5,  wxColour(0x00, 0x00, 0x7f));
-    this->StyleSetForeground(6,  wxColour(0x7f, 0x00, 0x7f));
-    this->StyleSetForeground(7,  wxColour(0x7f, 0x00, 0x7f));
-    this->StyleSetForeground(8,  wxColour(0x00, 0x7f, 0x7f));
-    this->StyleSetForeground(9,  wxColour(0x7f, 0x7f, 0x7f));
-    this->StyleSetForeground(10, wxColour(0x00, 0x00, 0x00));
-    this->StyleSetForeground(11, wxColour(0x00, 0x00, 0x00));
+    StyleSetForeground(0,  wxColour(0x80, 0x80, 0x80));
+    StyleSetForeground(1,  wxColour(0x00, 0x7f, 0x00));
+    StyleSetForeground(2,  wxColour(0x00, 0x7f, 0x00));
+    StyleSetForeground(3,  wxColour(0x7f, 0x7f, 0x7f));
+    StyleSetForeground(4,  wxColour(0x00, 0x7f, 0x7f));
+    StyleSetForeground(5,  wxColour(0x00, 0x00, 0x7f));
+    StyleSetForeground(6,  wxColour(0x7f, 0x00, 0x7f));
+    StyleSetForeground(7,  wxColour(0x7f, 0x00, 0x7f));
+    StyleSetForeground(8,  wxColour(0x00, 0x7f, 0x7f));
+    StyleSetForeground(9,  wxColour(0x7f, 0x7f, 0x7f));
+    StyleSetForeground(10, wxColour(0x00, 0x00, 0x00));
+    StyleSetForeground(11, wxColour(0x00, 0x00, 0x00));
     
     // SQL Lexer and keywords.
-    this->SetLexer(wxSTC_LEX_SQL);
-    this->SetKeyWords(0, SQL_KEYWORDS);
+    SetLexer(wxSTC_LEX_SQL);
+    SetKeyWords(0, SQL_KEYWORDS);
 
-    // Font
-    wxFont fntSQLBox(10, wxMODERN, wxNORMAL, wxNORMAL);
-    this->StyleSetFont(wxSTC_STYLE_DEFAULT, fntSQLBox);
     
 }
 
