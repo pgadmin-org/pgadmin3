96358ffe2351f82c1326800bb28392a12fd2acbe
Keith <efesar@nmia.com>
2003-04-07 22:01:21 +0000

    Subclasses some objects, including the column list and the drag/drop class.
    git-svn-id: svn://svn.pgadmin.org/trunk@968 a7884b65-44f6-0310-8a51-81a127f17b15

--- src/include/frmChildTableViewFrame.h
+++ src/include/frmChildTableViewFrame.h
@@ -10,6 +10,31 @@
 
 #ifndef frmChildfrmChildTableViewFrame_H
 #define frmChildfrmChildTableViewFrame_H
+		
+////////////////////////////////////////////////////////////////////////////////
+// Class Definition
+////////////////////////////////////////////////////////////////////////////////
+class myList : public wxListBox
+{
+
+public:
+
+	// Constructor
+	myList(wxWindow* parent, wxWindowID id) :
+	wxListBox(parent, id, wxDefaultPosition, 
+		wxDefaultSize, 0, NULL, wxLB_NEEDED_SB)
+	{
+		//
+	}
+
+private:
+
+	// Events
+    void OnMotion(wxMouseEvent& event);
+
+	// Macros
+	DECLARE_EVENT_TABLE()
+};
 
 ////////////////////////////////////////////////////////////////////////////////
 // Class Definition
@@ -22,21 +47,25 @@ public:
 	~frmChildTableViewFrame();
 
 	// Controls
-	wxListBox *m_columnlist;
-
-private:
+	myList *m_columnlist;
 
 	// Data
+	wxString m_title;
 	pgDatabase *m_database;
+
+private:
 
 	// Controls
 	wxBoxSizer *m_sizer;
 
 	// Events
+	void OnMove(wxMoveEvent& event);
+	void OnSize(wxSizeEvent& event);
 	void OnDoubleClick(wxCommandEvent& event);
 	void OnClose();
 	void OnCloseWindow(wxCloseEvent& event);
 	void OnAddColumn();
+	void OnJoinTo(wxCommandEvent& event);
 
 #ifdef __WXMSW__
     void OnContextMenu(wxContextMenuEvent& event)
@@ -58,11 +87,38 @@ private:
 	enum
 	{
 		MNU_ADDCOLUMN = 2000,
-		MNU_CLOSE = 2001
+		MNU_CLOSE
+	};
+
+	// "Reserve" these IDs for dynamic menus
+	enum
+	{
+		MNU_JOINTO = 3000,
+		MNU_JOINTO_N = 3999
 	};
 
 	// Macros
 	DECLARE_EVENT_TABLE()
 };
 
-#endif
+////////////////////////////////////////////////////////////////////////////////
+// Class Definition
+////////////////////////////////////////////////////////////////////////////////
+class DnDJoin : public wxTextDropTarget
+{
+
+public:
+
+	DnDJoin(wxFrame *frame) 
+	{ 
+		m_frame = frame; 
+	}
+
+    virtual bool OnDropText(wxCoord x, wxCoord y, const wxString& text);
+	
+private:
+
+    wxFrame *m_frame;
+};
+
+#endif // frmChildfrmChildTableViewFrame_H
