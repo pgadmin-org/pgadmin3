4bca716a0923032bfa44baf1e11f64f182be1751
Dave Page <dpage@pgadmin.org>
2003-09-02 15:10:06 +0000

    Post release version bump.
    git-svn-id: svn://svn.pgadmin.org/trunk@2316 a7884b65-44f6-0310-8a51-81a127f17b15

--- src/include/version.h
+++ src/include/version.h
@@ -10,8 +10,8 @@
 
 
 // Application Versions
-#define VERSION_STR wxT("0.9.2 BETA 2")
-#define VERSION_NUM 0,9,2,0
+#define VERSION_STR wxT("0.9.3 Devel")
+#define VERSION_NUM 0,9,3,0
 #define PRERELEASE 1
 // #define BUILD "..."
 
