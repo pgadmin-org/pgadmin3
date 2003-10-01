cfd3f61115caf7e347dca0e0aae9ae4c7a202082
Andreas Pflug <pgadmin@pse-consulting.de>
2003-10-01 11:44:44 +0000

    utffile
    git-svn-id: svn://svn.pgadmin.org/trunk@2673 a7884b65-44f6-0310-8a51-81a127f17b15

---
+++ src/include/utffile.h
@@ -0,0 +1,47 @@
+//////////////////////////////////////////////////////////////////////////
+//
+// pgAdmin III - PostgreSQL Tools
+// Copyright (C) 2002 - 2003, The pgAdmin Development Team
+// This software is released under the Artistic Licence
+//
+// utffile.h - file io with BOM interpretation
+//
+//////////////////////////////////////////////////////////////////////////
+
+
+#ifndef _WX_UTFFILEH__
+#define _WX_UTFFILEH__
+
+#include  "wx/file.h"
+
+class wxUtfFile : public wxFile
+{
+public:
+    wxUtfFile();
+    wxUtfFile(const wxChar *szFileName, OpenMode mode = read, wxFontEncoding encoding=wxFONTENCODING_DEFAULT);
+    wxUtfFile(int fd, wxFontEncoding encoding=wxFONTENCODING_DEFAULT);
+
+    bool Create(const wxChar *szFileName, bool bOverwrite = FALSE, int access = wxS_DEFAULT, wxFontEncoding encoding=wxFONTENCODING_DEFAULT);
+    bool Open(const wxChar *szFileName, OpenMode mode = read, int access = wxS_DEFAULT, wxFontEncoding encoding=wxFONTENCODING_DEFAULT);
+    void Attach(int fd, wxFontEncoding encoding=wxFONTENCODING_DEFAULT);
+
+    off_t Seek(off_t ofs, wxSeekMode mode = wxFromStart);
+    off_t SeekEnd(off_t ofs = 0) { return Seek(ofs, wxFromEnd); }
+    off_t Tell() const { return wxFile::Tell() - m_bomOffset; }
+    off_t Length() const { return wxFile::Length() - m_bomOffset; }
+
+    off_t Read(wxString& str, off_t nCount=(off_t)-1);
+    bool Write(const wxString& str);
+
+protected:
+
+    void WriteBOM();
+    void DetermineConversion(wxFontEncoding encoding);
+    bool EvalBOM(wxFontEncoding encoding);
+
+    wxMBConv *m_conversion;
+    wxFontEncoding m_encoding;
+    off_t m_bomOffset;
+};
+
+#endif // _WX_UTFFILEH__
