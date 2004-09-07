a4f27624b225adcc515fc803e1f7f063d155626e
Andreas Pflug <pgadmin@pse-consulting.de>
2004-09-07 08:38:13 +0000

    drop frmOptions:txtSearchPath
    git-svn-id: svn://svn.pgadmin.org/trunk@3625 a7884b65-44f6-0310-8a51-81a127f17b15

--- src/ui/frmOptions.cpp
+++ src/ui/frmOptions.cpp
@@ -46,7 +46,6 @@ extern wxArrayString existingLangNames;
 #define chkShowUsersForPrivileges   CTRL_CHECKBOX("chkShowUsersForPrivileges")
 #define txtAutoRowCount             CTRL_TEXT("txtAutoRowCount")
 #define chkStickySql                CTRL_CHECKBOX("chkStickySql")
-#define txtSearchPath               CTRL_TEXT("txtSearchPath")
 #define chkDoubleClickProperties    CTRL_CHECKBOX("chkDoubleClickProperties")
 #define cbLanguage                  CTRL_COMBOBOX("cbLanguage")
 #define txtSqlFont                  CTRL_TEXT("txtSqlFont")
@@ -95,7 +94,6 @@ frmOptions::frmOptions(frmMain *parent)
     chkShowUsersForPrivileges->SetValue(settings->GetShowUsersForPrivileges());
     txtAutoRowCount->SetValue(NumToStr(settings->GetAutoRowCountThreshold()));
     chkStickySql->SetValue(settings->GetStickySql());
-    txtSearchPath->SetValue(settings->GetSearchPath());
     chkDoubleClickProperties->SetValue(settings->GetDoubleClickProperties());
     txtSqlHelpSite->SetValue(settings->GetSqlHelpSite());
     txtProxy->SetValue(settings->GetProxy());
@@ -187,7 +185,6 @@ void frmOptions::OnOK(wxCommandEvent &ev)
     settings->SetStickySql(chkStickySql->GetValue());
     settings->SetDoubleClickProperties(chkDoubleClickProperties->GetValue());
     settings->SetUnicodeFile(chkUnicodeFile->GetValue());
-    settings->SetSearchPath(txtSearchPath->GetValue());
     settings->SetFont(currentFont);
     settings->SetSQLFont(currentSqlFont);
 
