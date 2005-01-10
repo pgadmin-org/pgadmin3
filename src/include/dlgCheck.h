9c834ceee49c8038aac2f0ad6564d2b4ac99a0bf
Dave Page <dpage@pgadmin.org>
2005-01-10 15:26:30 +0000

    Update copyright
    git-svn-id: svn://svn.pgadmin.org/trunk@3942 a7884b65-44f6-0310-8a51-81a127f17b15

--- src/include/dlgCheck.h
+++ src/include/dlgCheck.h
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////
 //
 // pgAdmin III - PostgreSQL Tools
-// Copyright (C) 2002 - 2004, The pgAdmin Development Team
+// Copyright (C) 2002 - 2005, The pgAdmin Development Team
 // This software is released under the Artistic Licence
 //
 // dlgCheck.h - Check property 
--- src/include/dlgClasses.h
+++ src/include/dlgClasses.h
@@ -2,7 +2,7 @@
 //
 // pgAdmin III - PostgreSQL Tools
 // RCS-ID:      $Id$
-// Copyright (C) 2002 - 2004, The pgAdmin Development Team
+// Copyright (C) 2002 - 2005, The pgAdmin Development Team
 // This software is released under the Artistic Licence
 //
 // dlgClasses.h - Some dialogue base classes 
