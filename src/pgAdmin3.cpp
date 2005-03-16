5468806fd3d95b05feb8016ba159952ca4dd23c6
Dave Page <dpage@pgadmin.org>
2005-03-16 21:51:42 +0000

    I wanted to build a .app bundle for pgadmin, but I found no support for this in the build system. I added an option --build-bundle that basically overwrites --prefix to $(pwd)tmp, and added a PgAdminIII.app target to Makefile.am. The built bundle includes the pgadmin3 binary (named PgAdminIII, all shared libraries that binary depends on (system libs in /usr/lib and /System are excluded), the documentation and the ui/ folder. This code is in pgadmin3-buildbundle.diff
    I added code to pgAdmin3.cpp to find the ui and doc folder inside the
    bundle, using the SystemPathsCF class from wx (only when __WXMAC__, of
    course). Using just SystemPaths would be cleaner, as it should default
    to sensible locations on all plattforms, but as of wxmac 2.5.4,
    SystemPaths refers to the unix-implemenation, not to the osx
    implemenation. This code is in pgadmin3-usebundle.diff
    [Florian G. Pflug]
    
    
    git-svn-id: svn://svn.pgadmin.org/trunk@4030 a7884b65-44f6-0310-8a51-81a127f17b15

--- src/pgAdmin3.cpp
+++ src/pgAdmin3.cpp
@@ -20,6 +20,7 @@
 #include <wx/imagjpeg.h>
 #include <wx/imaggif.h>
 #include <wx/imagpng.h>
+#include <wx/stdpaths.h>
 
 // Windows headers
 #ifdef __WXMSW__
@@ -87,6 +88,8 @@ bool dialogTestMode=false;
 #define DOC_DIR     wxT("/docs")
 #define UI_DIR      wxT("/ui")
 #define COMMON_DIR  wxT("/common")
+#define SCRIPT_DIR  wxT("/scripts")
+#define HELPER_DIR  wxT("/helper")
 #define LANG_FILE   wxT("pgadmin3.lng")
 
 
@@ -171,7 +174,7 @@ bool pgAdmin3::OnInit()
 
     // evaluate all working paths
 
-#ifdef __WXMSW__
+#if defined(__WXMSW__)
 
     backupExecutable  = path.FindValidPath(wxT("pg_dump.exe"));
     restoreExecutable = path.FindValidPath(wxT("pg_restore.exe"));
@@ -186,6 +189,33 @@ bool pgAdmin3::OnInit()
     else
         docPath = loadPath + wxT("/../..") DOC_DIR;
     
+#elif defined(__WXMAC__)
+
+    //When using wxStandardPaths on OSX, wx defaults to the unix,
+    //not to the mac variants. Therefor, we request wxStandardPathsCF
+    //directly.
+    wxStandardPathsCF stdPaths ;
+    wxString dataDir = stdPaths.GetDataDir() ;
+    if (dataDir) {
+        wxFprintf(stderr, wxT("DataDir: ") + dataDir + wxT("\n")) ;
+	if (wxDir::Exists(dataDir + HELPER_DIR))
+            path.Add(dataDir + HELPER_DIR) ;
+        if (wxDir::Exists(dataDir + SCRIPT_DIR))
+            path.Add(dataDir + SCRIPT_DIR) ;
+        if (wxDir::Exists(dataDir + UI_DIR))
+          uiPath = dataDir + UI_DIR ;
+        if (wxDir::Exists(dataDir + DOC_DIR))
+          docPath = dataDir + DOC_DIR ;
+    }
+
+    if (uiPath.IsEmpty())
+        uiPath = loadPath + UI_DIR ;
+    if (docPath.IsEmpty())
+        docPath = loadPath + wxT("/..") DOC_DIR ;
+
+    backupExecutable  = path.FindValidPath(wxT("pg_dump"));
+    restoreExecutable = path.FindValidPath(wxT("pg_restore"));
+
 #else
 
     backupExecutable  = path.FindValidPath(wxT("pg_dump"));
@@ -200,6 +230,7 @@ bool pgAdmin3::OnInit()
         docPath = DATA_DIR DOC_DIR;
     else
         docPath = loadPath + wxT("/..") DOC_DIR;
+
 #endif
 
 
