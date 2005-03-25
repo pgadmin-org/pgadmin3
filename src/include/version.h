a01ce4e1853f62dc5fe80c1dcffabff9f6099046
Dave Page <dpage@pgadmin.org>
2005-03-25 22:00:03 +0000

    Version number bump for 1.2.1
    git-svn-id: svn://svn.pgadmin.org/branches/REL-1_2_0_PATCHES@4041 a7884b65-44f6-0310-8a51-81a127f17b15

--- src/include/version.h
+++ src/include/version.h
@@ -11,9 +11,9 @@
 
 
 // Application Versions
-#define VERSION_STR     wxT("1.2.0")
-#define VERSION_NUM     1,2,0,0
-#define VERSION_PACKAGE 1.2.0
+#define VERSION_STR     wxT("1.2.1")
+#define VERSION_NUM     1,2,1,0
+#define VERSION_PACKAGE 1.2.1
 
 #define PRERELEASE 0
 // #define BUILD "..."
