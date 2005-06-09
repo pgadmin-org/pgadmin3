e63fb4047840fdf65daf8043097a06a82a133641
Dave Page <dpage@pgadmin.org>
2005-06-09 14:45:20 +0000

    Check the correct object for the cluster name.
    git-svn-id: svn://svn.pgadmin.org/trunk@4293 a7884b65-44f6-0310-8a51-81a127f17b15

--- src/slony/dlgRepCluster.cpp
+++ src/slony/dlgRepCluster.cpp
@@ -617,7 +617,7 @@ void dlgRepCluster::CheckChange()
             (!slony1BaseScript.IsEmpty() && !slony1FunctionScript.IsEmpty() && !slony1XxidScript.IsEmpty()),
             _("Slony-I creation scripts not available; only joining possible."));
 
-        CheckValid(enable, !cbClusterName->GetValue().IsEmpty(), _("Please specify name."));
+        CheckValid(enable, !txtClusterName->GetValue().IsEmpty(), _("Please specify name."));
 
         long nodeId = StrToLong(txtNodeID->GetValue());
         CheckValid(enable, nodeId > 0, _("Please specify local node ID."));
