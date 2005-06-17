d692aadb759942b9da7a13c1a9e922abe9ddc96f
Andreas Pflug <pgadmin@pse-consulting.de>
2005-06-17 17:17:21 +0000

    fix typo
    git-svn-id: svn://svn.pgadmin.org/trunk@4312 a7884b65-44f6-0310-8a51-81a127f17b15

--- xtra/pgagent/pgagent.sql
+++ xtra/pgagent/pgagent.sql
@@ -63,7 +63,7 @@ jstdesc              text                 NOT NULL DEFAULT '',
 jstenabled           bool                 NOT NULL DEFAULT true,
 jstkind              char                 NOT NULL CHECK (jstkind IN ('b', 's')), -- batch, sql
 jstcode              text                 NOT NULL,
-jstdbname            name                 NOT NULL DEFAULT '' CHECK ((jstdbname != '' AND jstkind = 's') OR (jstdbname = '' AND jstkind = 'b'))
+jstdbname            name                 NOT NULL DEFAULT '' CHECK ((jstdbname != '' AND jstkind = 's') OR (jstdbname = '' AND jstkind = 'b')),
 jstonerror           char                 NOT NULL CHECK (jstonerror IN ('f', 's', 'i')) DEFAULT 'f', -- fail, success, ignore
 jscnextrun           timestamptz          NULL
 ) WITHOUT OIDS;
