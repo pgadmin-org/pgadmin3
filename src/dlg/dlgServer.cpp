9c9f9a1b2954c51be5640deaea6779b5c8277b0f
Andreas Pflug <pgadmin@pse-consulting.de>
2005-06-17 13:03:40 +0000

    Fix changing initial db
    git-svn-id: svn://svn.pgadmin.org/trunk@4310 a7884b65-44f6-0310-8a51-81a127f17b15

--- src/dlg/dlgServer.cpp
+++ src/dlg/dlgServer.cpp
@@ -103,7 +103,7 @@ void dlgServer::OnOK(wxCommandEvent &ev)
         }
         server->iSetPort(StrToLong(txtPort->GetValue()));
         server->iSetSSL(cbSSL->GetSelection());
-        server->iSetLastDatabase(cbDatabase->GetValue());
+        server->iSetDatabase(cbDatabase->GetValue());
         server->iSetUsername(txtUsername->GetValue());
         server->iSetNeedPwd(chkNeedPwd->GetValue());
         mainForm->execSelChange(server->GetId(), true);
