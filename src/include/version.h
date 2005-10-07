0051b444264a42829871685626f0667e42a58667
Dave Page <dpage@pgadmin.org>
2005-10-07 08:27:23 +0000

    Version tweaks to avoid confusion
    git-svn-id: svn://svn.pgadmin.org/trunk@4517 a7884b65-44f6-0310-8a51-81a127f17b15

--- src/include/version.h
+++ src/include/version.h
@@ -11,9 +11,9 @@
 
 
 // Application Versions
-#define VERSION_STR     wxT("1.4.0 Beta 1")
+#define VERSION_STR     wxT("1.4.0 Beta 1 Dev 1")
 #define VERSION_NUM     1,4,0,0
-#define VERSION_PACKAGE 1.4.0-beta1
+#define VERSION_PACKAGE 1.4.0-beta1-dev1
 
 #define PRERELEASE 1
 // #define BUILD "..."
