2869e028aa2cfb5d9b1b225d31a6ce6a03f6c253
Dave Page <dpage@pgadmin.org>
2005-12-10 14:43:55 +0000

    Add an upgrade script
    git-svn-id: svn://svn.pgadmin.org/branches/REL-1_4_0_PATCHES@4830 a7884b65-44f6-0310-8a51-81a127f17b15

---
+++ pkg/win32/upgrade.bat
@@ -0,0 +1,23 @@
+@echo off
+cls
+echo This file will upgrade your pgAdmin III installation.
+echo.
+echo You must have pgAdmin III v1.4.x installed 
+echo from the official MSI installation to use this upgrade path.
+echo.
+echo If pgAdmin III or any of it's components are in use
+echo a reboot will be required once the upgrade is completed.
+echo.
+echo.
+echo Press Ctrl-C to abort the upgrade or
+pause
+
+REM Parameters described:
+REM  /i pgadmin3.msi           - pick MSI file to install. All properties
+REM                              will be read from existing installation.
+REM  REINSTALLMODE=vamus       - reinstall all files, regardless of version.
+REM                              This makes sure documentation and other
+REM                              non-versioned files are updated.
+REM  REINSTALL=ALL             - Reinstall all features that were previously
+REM                              installed with the new version.
+msiexec /i pgadmin3.msi REINSTALLMODE=vamus REINSTALL=ALL /qr
