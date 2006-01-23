08829590a296d7b45b4c4d6162ed7edb94ef958e
Dave Page <dpage@pgadmin.org>
2006-01-23 20:21:03 +0000

    Use wxConvLibc not wxConvUtf8, per Andreas
    git-svn-id: svn://svn.pgadmin.org/trunk@4953 a7884b65-44f6-0310-8a51-81a127f17b15

--- src/frm/frmConfig.cpp
+++ src/frm/frmConfig.cpp
@@ -312,7 +312,7 @@ void frmConfig::OpenLastFile()
         buffer[cnt] = 0;
         file.Close();
 
-        DisplayFile(wxString(buffer, wxConvUTF8));
+        DisplayFile(wxString(buffer, wxConvLibc));
 
         delete buffer;
         statusBar->SetStatusText(wxString::Format(_(" Configuration read from %s"), lastPath.c_str()));
