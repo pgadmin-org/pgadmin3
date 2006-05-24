58c31df6b0cff53db3338dd4d62b07c88cdb8f56
Dave Page <dpage@pgadmin.org>
2006-05-24 19:35:28 +0000

    Keep up 007\!
    git-svn-id: svn://svn.pgadmin.org/trunk@5198 a7884b65-44f6-0310-8a51-81a127f17b15

--- src/include/ctlSecurityPanel.h
+++ src/include/ctlSecurityPanel.h
@@ -30,7 +30,7 @@ enum
     CTL_PRIVCB          // base for all privilege checkboxes, must be last
 };
 
-DEFINE_EVENT_TYPE(EVT_SECURITYPANEL_CHANGE)
+DECLARE_EVENT_TYPE(EVT_SECURITYPANEL_CHANGE, -1)
 
 class pgConn;
 
