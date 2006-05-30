da5c52ab4bcc8d7eabec8ae0da8d03793d2d26b9
Dave Page <dpage@pgadmin.org>
2006-05-30 19:11:00 +0000

    Remove some headers that aren't required
    git-svn-id: svn://svn.pgadmin.org/trunk@5218 a7884b65-44f6-0310-8a51-81a127f17b15

--- src/frm/frmReport.cpp
+++ src/frm/frmReport.cpp
@@ -34,15 +34,6 @@
 #include "pgCheck.h"
 
 // XML2/XSLT headers
-#include <libxml/xmlmemory.h>
-#include <libxml/debugXML.h>
-#include <libxml/HTMLtree.h>
-#include <libxml/xmlIO.h>
-#include <libxml/DOCBparser.h>
-#include <libxml/xinclude.h>
-#include <libxml/catalog.h>
-#include <libxslt/xslt.h>
-#include <libxslt/xsltInternals.h>
 #include <libxslt/transform.h>
 #include <libxslt/xsltutils.h>
 
