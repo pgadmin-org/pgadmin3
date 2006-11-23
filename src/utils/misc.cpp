eff883701e448910157ebd7793d0482071ebb02a
Dave Page <dpage@pgadmin.org>
2006-11-23 16:08:10 +0000

    Strip carriage returns from report output (use line feed only).
    git-svn-id: svn://svn.pgadmin.org/trunk@5682 a7884b65-44f6-0310-8a51-81a127f17b15

--- src/utils/misc.cpp
+++ src/utils/misc.cpp
@@ -592,10 +592,11 @@ wxString GetHtmlEntity(const wxChar ch)
 wxString HtmlEntities(const wxString &str)
 {
     wxString ret;
-
+    
     for (unsigned int x=0; x < str.Length(); x++)
     {
-        ret += GetHtmlEntity(str[x]);
+        if (str[x] != 13)
+            ret += GetHtmlEntity(str[x]);
     }
 
     return ret;
