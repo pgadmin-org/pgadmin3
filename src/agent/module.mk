b0a704eb09a641a7470d53879b532d6e5c5d5fca
Dave Page <dpage@pgadmin.org>
2006-12-07 14:49:47 +0000

    Remove old module
    git-svn-id: svn://svn.pgadmin.org/trunk@5765 a7884b65-44f6-0310-8a51-81a127f17b15

--- src/agent/module.mk
+++ src/agent/module.mk
@@ -10,15 +10,14 @@
 #######################################################################
 
 pgadmin3_SOURCES += \
-	$(srcdir)/agent/dlgJob.cpp \
-	$(srcdir)/agent/dlgSchedule.cpp \
-	$(srcdir)/agent/dlgStep.cpp \
-	$(srcdir)/agent/pgaJob.cpp \
-	$(srcdir)/agent/pgaSchedule.cpp \
-	$(srcdir)/agent/pgaStep.cpp
+    $(srcdir)/agent/dlgJob.cpp \
+	  $(srcdir)/agent/dlgSchedule.cpp \
+	  $(srcdir)/agent/dlgStep.cpp \
+	  $(srcdir)/agent/pgaJob.cpp \
+	  $(srcdir)/agent/pgaSchedule.cpp \
+	  $(srcdir)/agent/pgaStep.cpp
 
 EXTRA_DIST += \
-        $(srcdir)/agent/module.mk
+    $(srcdir)/agent/module.mk
 
-include $(srcdir)/agent/include/module.mk
 
