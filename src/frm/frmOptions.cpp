4aadbdb1aff267593b7f2a90fa59c546ea3b1c64
Dave Page <dpage@pgadmin.org>
2007-01-19 17:34:12 +0000

    Fix localisation of 'Tab' as a copy delimiter.
    git-svn-id: svn://svn.pgadmin.org/branches/REL-1_6_0_PATCHES@5879 a7884b65-44f6-0310-8a51-81a127f17b15

--- src/frm/frmOptions.cpp
+++ src/frm/frmOptions.cpp
@@ -112,7 +112,7 @@ frmOptions::frmOptions(frmMain *parent)
 
     wxString copySeparator = settings->GetCopyColSeparator();
     if (copySeparator == wxT("\t"))
-        copySeparator = wxT("Tab");
+        copySeparator = _("Tab");
     cbCopySeparator->SetValue(copySeparator);
 
 	chkTabForCompletion->SetValue(settings->GetTabForCompletion());
@@ -236,7 +236,7 @@ void frmOptions::OnOK(wxCommandEvent &ev)
 	settings->SetCopyQuoteChar(cbCopyQuoteChar->GetValue());
 	
     wxString copySeparator = cbCopySeparator->GetValue();
-    if (copySeparator == wxT("Tab"))
+    if (copySeparator == _("Tab"))
         copySeparator = wxT("\t");
     settings->SetCopyColSeparator(copySeparator);
 
