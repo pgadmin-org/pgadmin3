51f588cc29916df842156de92afedee22232140b
Dave Page <dpage@pgadmin.org>
2007-02-25 09:23:09 +0000

    Fix some copyright notices
    git-svn-id: svn://svn.pgadmin.org/trunk@5942 a7884b65-44f6-0310-8a51-81a127f17b15

--- xtra/pgaevent/pgamsgevent.h
+++ xtra/pgaevent/pgamsgevent.h
@@ -2,7 +2,7 @@
 //
 // pgAgent - PostgreSQL Tools
 // $Id: pgamsgevent.h 4875 2006-01-06 21:06:46Z dpage $
-// Copyright (C) 2002 - 2006 The pgAdmin Development Team
+// Copyright (C) 2002 - 2007, The pgAdmin Development Team
 // This software is released under the Artistic Licence
 //
 // pgamsgevent.h - Message id declarations
