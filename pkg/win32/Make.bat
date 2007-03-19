aca7173248f1b3a89149fe5427e64fdb4e37d1e7
Dave Page <dpage@pgadmin.org>
2007-03-19 14:05:20 +0000

    Set icons properly. Allow easier rebranding.
    git-svn-id: svn://svn.pgadmin.org/trunk@6104 a7884b65-44f6-0310-8a51-81a127f17b15

--- pkg/win32/Make.bat
+++ pkg/win32/Make.bat
@@ -1,9 +1,28 @@
 @echo off
 
+REM
+REM Setup the following values as required for the installation
+REM
+
+SET APPVENDOR=The pgAdmin Development Team
+SET APPNAME=pgAdmin III
+SET APPKEYWORDS=PostgreSQL, pgAdmin
+SET APPCOMMENTS=PostgreSQL Tools
+SET APPDESCRIPTION=Management and administration tools for the PostgreSQL DBMS
+
+SET BUILDTREE="../.."
+
+SET BRANDED=0
+SET BRANDINGDIR="../../branding"
+
+REM
+REM END OF CONFIG SECTION
+REM
+
 if NOT "%1"=="" GOTO REGEN_GUIDS
 
 echo Invalid command line options.
-echo Usage: "Make.bat <version number>"
+echo Usage: "Make.bat <Major.Minor version number>"
 echo        "Make.bat REGENGUIDS"
 echo.
 GOTO EXIT
@@ -34,9 +53,9 @@ GOTO EXIT
 :BUILD_PACKAGE
 
 echo.
-echo Building pgAdmin III Installer...
+echo Building %APPNAME% Installer...
 
-candle -nologo -dAPPNAME="pgAdmin III" -dVERSION="%1" -dSYSTEM32DIR="%SystemRoot%\System32" -dPFILESDIR="%ProgramFiles%" src/pgadmin3.wxs
+candle -nologo -dBUILDTREE="%BUILDTREE%" -dBRANDED=%BRANDED% -dBRANDINGDIR="%BRANDINGDIR%" -dAPPVENDOR="%APPVENDOR%" -dAPPNAME="%APPNAME%" -dAPPKEYWORDS="%APPKEYWORDS%" -dAPPCOMMENTS="%APPCOMMENTS%" -dAPPDESCRIPTION="%APPDESCRIPTION%" -dAPPVERSION="%1" -dSYSTEM32DIR="%SystemRoot%\System32" -dPFILESDIR="%ProgramFiles%" src/pgadmin3.wxs
 IF ERRORLEVEL 1 GOTO ERR_HANDLER
 
 light -nologo -ext WixUIExtension -cultures:en-us pgadmin3.wixobj
