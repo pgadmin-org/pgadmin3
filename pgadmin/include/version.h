8069ff28bcac60e86ad62f16609eea75cfbb476e
Dave Page <dpage@pgadmin.org>
2007-10-19 13:19:15 +0000

    Bump version for the first patch release
    git-svn-id: svn://svn.pgadmin.org/branches/REL-1_8_0_PATCHES@6774 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin/include/version.h
+++ pgadmin/include/version.h
@@ -13,9 +13,9 @@
 #define VERSION_H
 
 // Application Versions
-#define VERSION_STR     wxT("1.8.0")
-#define VERSION_NUM     1,8,0,99
-#define VERSION_PACKAGE 1.8.0
+#define VERSION_STR     wxT("1.8.1")
+#define VERSION_NUM     1,8,1,99
+#define VERSION_PACKAGE 1.8.1
 
 #define PRERELEASE 0
 // #define BUILD "..."
