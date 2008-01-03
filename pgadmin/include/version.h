a28bf3f2c47d6f6a3305ef7925b6a6bf4e1fb9c4
Dave Page <dpage@pgadmin.org>
2008-01-03 15:19:59 +0000

    Post-release version number bump
    git-svn-id: svn://svn.pgadmin.org/branches/REL-1_8_0_PATCHES@6951 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin/include/version.h
+++ pgadmin/include/version.h
@@ -13,9 +13,9 @@
 #define VERSION_H
 
 // Application Versions
-#define VERSION_STR     wxT("1.8.1")
-#define VERSION_NUM     1,8,1,99
-#define VERSION_PACKAGE 1.8.1
+#define VERSION_STR     wxT("1.8.2 - Development")
+#define VERSION_NUM     1,8,2,50
+#define VERSION_PACKAGE 1.8.2-dev
 
 #define PRERELEASE 0
 // #define BUILD "..."
