d7fc76155dc10261fd9b286f88c6c68770b39dfa
Dave Page <dpage@pgadmin.org>
2008-01-18 13:46:12 +0000

    Fix the WITH ADMIN membership option when creating roles.
    git-svn-id: svn://svn.pgadmin.org/branches/REL-1_8_0_PATCHES@7002 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin/dlg/dlgRole.cpp
+++ pgadmin/dlg/dlgRole.cpp
@@ -620,19 +620,32 @@ wxString dlgRole::GetSql()
             sql += wxT("\n   VALID UNTIL 'infinity'");
         
         int cnt = lbRolesIn->GetCount();
-        int pos;
+		wxString grants;
 
         if (cnt)
         {
-            sql += wxT("\n   IN ROLE ");
-            for (pos=0 ; pos < cnt ; pos++)
+			wxString roleName;
+
+            for (int pos=0 ; pos < cnt ; pos++)
             {
-                if (pos)
-                    sql += wxT(", ");
-                sql += qtIdent(lbRolesIn->GetString(pos));
+                bool admin = false;    
+				roleName = lbRolesIn->GetString(pos);
+				if (roleName.Right(PGROLE_ADMINOPTION_LEN) == PGROLE_ADMINOPTION)
+				{
+					roleName=roleName.Left(roleName.Length()-PGROLE_ADMINOPTION_LEN);
+					admin = true;
+
+				}
+				grants += wxT("GRANT ") + qtIdent(roleName)
+					   +  wxT(" TO ") + qtIdent(name);
+
+				if (admin)
+			        grants += wxT(" WITH ADMIN OPTION;\n");
+				else
+					grants += wxT(";\n");
             }
         }
-        sql += wxT(";\n");
+        sql += wxT(";\n") + grants;
 
         if (superuser && !chkUpdateCat->GetValue())
             sql += wxT("UPDATE pg_authid SET rolcatupdate=false WHERE rolname=") + qtDbString(name) + wxT(";\n");
