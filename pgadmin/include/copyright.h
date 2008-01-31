ad342bacf6d9eab9f363e2105b372310e4b636ca
Dave Page <dpage@pgadmin.org>
2008-01-31 13:31:39 +0000

    Shorten the copyright text which is overflowing the splash screen on GTK.
    git-svn-id: svn://svn.pgadmin.org/trunk@7025 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin/include/copyright.h
+++ pgadmin/include/copyright.h
@@ -12,7 +12,7 @@
 #ifndef COPYRIGHT_H
 #define COPYRIGHT_H
 
-#define COPYRIGHT               wxT("Copyright (C) 2002 - 2008, The pgAdmin Development Team")
+#define COPYRIGHT               wxT("Copyright 2002 - 2008, The pgAdmin Development Team")
 #define COPYRIGHT_WIN32         "\251 2002 - 2008, The pgAdmin Development Team"
 #define LICENSE                 _("This software is released under the Artistic License.")
 
