18aecaf17a6f441f52feff3c05be3f1c6f2a47db
Dave Page <dpage@pgadmin.org>
2008-05-01 19:34:47 +0000

    Don't load group roles and old-style groups on the security tab on 8.1 and higher, per Jon Roberte. Correctly quote role names when reverse engineering ACLs.
    git-svn-id: svn://svn.pgadmin.org/trunk@7246 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin/dlg/dlgProperty.cpp
+++ pgadmin/dlg/dlgProperty.cpp
@@ -1379,6 +1379,9 @@ void dlgSecurityProperty::AddGroups(ctlComboBox *comboBox)
     if (!((securityPage && securityPage->cbGroups) || comboBox))
         return;
 
+	if (connection->BackendMinimumVersion(8, 1))
+		return;
+
     pgSet *set=connection->ExecuteSet(wxT("SELECT groname FROM pg_group ORDER BY groname"));
 
     if (set)
