e586c01d407a76080d2a156e872c09c2b05aacfd
Guillaume Lelarge <guillaume@lelarge.info>
2008-05-23 21:19:59 +0000

    Fix a wrong method declaration.
    git-svn-id: svn://svn.pgadmin.org/trunk@7308 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin/include/ctl/ctlTree.h
+++ pgadmin/include/ctl/ctlTree.h
@@ -26,7 +26,7 @@ class ctlTree : public wxTreeCtrl
 {
 public:
     ctlTree(wxWindow* parent, wxWindowID id, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize, long style = wxTR_HAS_BUTTONS);
-    wxTreeItemId ctlTree::AppendItem(const wxTreeItemId& parent, const wxString& text, int image = -1, int selImage = -1, wxTreeItemData* data = NULL);
+    wxTreeItemId AppendItem(const wxTreeItemId& parent, const wxString& text, int image = -1, int selImage = -1, wxTreeItemData* data = NULL);
 	wxTreeItemId AppendObject(pgObject *parent, pgObject *object);
     void RemoveDummyChild(pgObject *obj);
     pgCollection *AppendCollection(pgObject *parent, pgaFactory &factory);
