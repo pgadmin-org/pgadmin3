0c454ef487abcd6d90cbe76a2c54761e334d18ce
Dave Page <dpage@pgadmin.org>
2008-06-20 09:38:15 +0000

    Ensure that line endings stay consistent when editing in styled text controls.
    git-svn-id: svn://svn.pgadmin.org/branches/REL-1_8_0_PATCHES@7379 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin/frm/frmQuery.cpp
+++ pgadmin/frm/frmQuery.cpp
@@ -1378,6 +1378,7 @@ void frmQuery::OnSetEOLMode(wxCommandEvent& event)
 {
     int mode = GetLineEndingStyle();
     sqlQuery->ConvertEOLs(mode);
+	sqlQuery->SetEOLMode(mode);
 
     switch(mode)
     {
