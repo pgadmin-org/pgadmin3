8d04d82dc7fda563832375f8b52b903b9e9332aa
Dave Page <dpage@pgadmin.org>
2009-11-26 12:02:39 +0000

    Prevent a crash if the edit grid is closed whilst it  is loading data.
    git-svn-id: svn://svn.pgadmin.org/trunk@8092 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin/frm/frmEditGrid.cpp
+++ pgadmin/frm/frmEditGrid.cpp
@@ -93,6 +93,8 @@ END_EVENT_TABLE()
 frmEditGrid::frmEditGrid(frmMain *form, const wxString& _title, pgConn *_conn, pgSchemaObject *obj)
 : pgFrame(NULL, _title)
 {
+    closing = false;
+
     SetIcon(wxIcon(viewdata_xpm));
     wxWindowBase::SetFont(settings->GetSystemFont());
     dlgName = wxT("frmEditGrid");
@@ -1302,6 +1304,10 @@ void frmEditGrid::Go()
         wxTheApp->Yield(true);
         wxMilliSleep(10);
     }
+
+    // Brute force check to ensure the user didn't get bored and close the window
+    if (closing)
+        return;
     
     if (!thread)
     {
@@ -1333,6 +1339,7 @@ void frmEditGrid::Go()
         toolsMenu->Enable(MNU_REMOVESORT, true);
         return;
     }
+
     SetStatusText(wxString::Format(wxPLURAL("%d row.", "%d rows.", thread->DataSet()->NumRows()), thread->DataSet()->NumRows()), 0);
 
     sqlGrid->BeginBatch();
@@ -1373,6 +1380,8 @@ void frmEditGrid::Go()
 
 frmEditGrid::~frmEditGrid()
 {
+    closing = true;
+
     mainForm->RemoveFrame(this);
 
     settings->Write(wxT("frmEditGrid/Perspective-") + VerFromRev(FRMEDITGRID_PERPSECTIVE_VER), manager.SavePerspective());
