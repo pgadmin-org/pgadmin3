pgadmin-org/pgadmin3

commit 9f37af85461006ef039bdeb404f7aaa19fcea767

Make sure that Statistics tab doesn't shows more databases than Properties tab.
Fixes #157


git-svn-id: svn://svn.pgadmin.org/branches/REL-1_10_0_PATCHES@8237 a7884b65-44f6-0310-8a51-81a127f17b15

pgadmin/schema/pgDatabase.cpp
@@ -743,14 +743,25 @@ void pgDatabaseCollection::ShowStatistics(frmMain *form, ctlListView *statistics
 
     bool hasSize=GetConnection()->HasFeature(FEATURE_SIZE);
 
-    wxString sql=wxT("SELECT datname, numbackends, xact_commit, xact_rollback, blks_read, blks_hit");
+    wxString restr;
+    if (!GetServer()->GetDbRestriction().IsEmpty())
+    {
+        if (restr.IsEmpty())
+            restr = wxT(" WHERE datname IN (");
+        else
+            restr = wxT("   AND datname IN (");
+
+        restr += GetServer()->GetDbRestriction() + wxT(")\n");
+    }
+    
+    wxString sql=wxT("SELECT datid, datname, numbackends, xact_commit, xact_rollback, blks_read, blks_hit");
 
 	if (GetConnection()->BackendMinimumVersion(8,3))
 		sql += wxT(", tup_returned, tup_fetched, tup_inserted, tup_updated, tup_deleted");
     if (hasSize)
         sql += wxT(", pg_size_pretty(pg_database_size(datid)) as size");
 
-    sql += wxT("\n  FROM pg_stat_database db ORDER BY datname");
+    sql += wxT("\n  FROM pg_stat_database db\n") + restr + wxT(" ORDER BY datname");
 
     // Add the statistics view columns
     statistics->ClearAll();
@@ -771,27 +782,36 @@ void pgDatabaseCollection::ShowStatistics(frmMain *form, ctlListView *statistics
 		statistics->AddColumn(_("Tuples Deleted"), 60);
 	}
 
+    bool sysobj;
     pgSet *stats = GetServer()->ExecuteSet(sql);
     if (stats)
     {
         while (!stats->Eof())
         {
-            statistics->InsertItem(stats->CurrentPos() - 1, stats->GetVal(wxT("datname")), PGICON_STATISTICS);
-            statistics->SetItem(stats->CurrentPos() - 1, 1, stats->GetVal(wxT("numbackends")));
-            if (hasSize)
-                statistics->SetItem(stats->CurrentPos() - 1, 2, stats->GetVal(wxT("size")));
-			statistics->SetItem(stats->CurrentPos() - 1, 2 + (hasSize?1:0), stats->GetVal(wxT("xact_commit")));
-            statistics->SetItem(stats->CurrentPos() - 1, 3 + (hasSize?1:0), stats->GetVal(wxT("xact_rollback")));
-            statistics->SetItem(stats->CurrentPos() - 1, 4 + (hasSize?1:0), stats->GetVal(wxT("blks_read")));
-            statistics->SetItem(stats->CurrentPos() - 1, 5 + (hasSize?1:0), stats->GetVal(wxT("blks_hit")));
-			if (GetConnection()->BackendMinimumVersion(8,3))
-			{
-				statistics->SetItem(stats->CurrentPos()-1, 6 + (hasSize?1:0), stats->GetVal(wxT("tup_returned")));
-				statistics->SetItem(stats->CurrentPos()-1, 7 + (hasSize?1:0), stats->GetVal(wxT("tup_fetched")));
-				statistics->SetItem(stats->CurrentPos()-1, 8 + (hasSize?1:0), stats->GetVal(wxT("tup_inserted")));
-				statistics->SetItem(stats->CurrentPos()-1, 9 + (hasSize?1:0), stats->GetVal(wxT("tup_updated")));
-				statistics->SetItem(stats->CurrentPos()-1, 10 + (hasSize?1:0), stats->GetVal(wxT("tup_deleted")));
-			}
+            if (stats->GetVal(wxT("datname")) == wxT("template0"))
+                sysobj = true;
+            else
+                sysobj = stats->GetOid(wxT("datid")) <= GetServer()->GetLastSystemOID();
+
+            if (settings->GetShowSystemObjects() || !sysobj)
+            {
+                statistics->InsertItem(stats->CurrentPos() - 1, stats->GetVal(wxT("datname")), PGICON_STATISTICS);
+                statistics->SetItem(stats->CurrentPos() - 1, 1, stats->GetVal(wxT("numbackends")));
+                if (hasSize)
+                    statistics->SetItem(stats->CurrentPos() - 1, 2, stats->GetVal(wxT("size")));
+			    statistics->SetItem(stats->CurrentPos() - 1, 2 + (hasSize?1:0), stats->GetVal(wxT("xact_commit")));
+                statistics->SetItem(stats->CurrentPos() - 1, 3 + (hasSize?1:0), stats->GetVal(wxT("xact_rollback")));
+                statistics->SetItem(stats->CurrentPos() - 1, 4 + (hasSize?1:0), stats->GetVal(wxT("blks_read")));
+                statistics->SetItem(stats->CurrentPos() - 1, 5 + (hasSize?1:0), stats->GetVal(wxT("blks_hit")));
+			    if (GetConnection()->BackendMinimumVersion(8,3))
+			    {
+			    	statistics->SetItem(stats->CurrentPos()-1, 6 + (hasSize?1:0), stats->GetVal(wxT("tup_returned")));
+			    	statistics->SetItem(stats->CurrentPos()-1, 7 + (hasSize?1:0), stats->GetVal(wxT("tup_fetched")));
+			    	statistics->SetItem(stats->CurrentPos()-1, 8 + (hasSize?1:0), stats->GetVal(wxT("tup_inserted")));
+			    	statistics->SetItem(stats->CurrentPos()-1, 9 + (hasSize?1:0), stats->GetVal(wxT("tup_updated")));
+			    	statistics->SetItem(stats->CurrentPos()-1, 10 + (hasSize?1:0), stats->GetVal(wxT("tup_deleted")));
+			    }
+            }
             stats->MoveNext();
         }
 

commit fe6d488c50282c9b810a66c40486e91f4e272751

Fix an issue I introduced in revision r8237.
git-svn-id: svn://svn.pgadmin.org/branches/REL-1_10_0_PATCHES@8255 a7884b65-44f6-0310-8a51-81a127f17b15

pgadmin/schema/pgDatabase.cpp
@@ -795,21 +795,21 @@ void pgDatabaseCollection::ShowStatistics(frmMain *form, ctlListView *statistics
 
             if (settings->GetShowSystemObjects() || !sysobj)
             {
-                statistics->InsertItem(stats->CurrentPos() - 1, stats->GetVal(wxT("datname")), PGICON_STATISTICS);
-                statistics->SetItem(stats->CurrentPos() - 1, 1, stats->GetVal(wxT("numbackends")));
+                statistics->InsertItem(statistics->GetItemCount(), stats->GetVal(wxT("datname")), PGICON_STATISTICS);
+                statistics->SetItem(statistics->GetItemCount() - 1, 1, stats->GetVal(wxT("numbackends")));
                 if (hasSize)
-                    statistics->SetItem(stats->CurrentPos() - 1, 2, stats->GetVal(wxT("size")));
-			    statistics->SetItem(stats->CurrentPos() - 1, 2 + (hasSize?1:0), stats->GetVal(wxT("xact_commit")));
-                statistics->SetItem(stats->CurrentPos() - 1, 3 + (hasSize?1:0), stats->GetVal(wxT("xact_rollback")));
-                statistics->SetItem(stats->CurrentPos() - 1, 4 + (hasSize?1:0), stats->GetVal(wxT("blks_read")));
-                statistics->SetItem(stats->CurrentPos() - 1, 5 + (hasSize?1:0), stats->GetVal(wxT("blks_hit")));
+                    statistics->SetItem(statistics->GetItemCount() - 1, 2, stats->GetVal(wxT("size")));
+			    statistics->SetItem(statistics->GetItemCount() - 1, 2 + (hasSize?1:0), stats->GetVal(wxT("xact_commit")));
+                statistics->SetItem(statistics->GetItemCount() - 1, 3 + (hasSize?1:0), stats->GetVal(wxT("xact_rollback")));
+                statistics->SetItem(statistics->GetItemCount() - 1, 4 + (hasSize?1:0), stats->GetVal(wxT("blks_read")));
+                statistics->SetItem(statistics->GetItemCount() - 1, 5 + (hasSize?1:0), stats->GetVal(wxT("blks_hit")));
 			    if (GetConnection()->BackendMinimumVersion(8,3))
 			    {
-			    	statistics->SetItem(stats->CurrentPos()-1, 6 + (hasSize?1:0), stats->GetVal(wxT("tup_returned")));
-			    	statistics->SetItem(stats->CurrentPos()-1, 7 + (hasSize?1:0), stats->GetVal(wxT("tup_fetched")));
-			    	statistics->SetItem(stats->CurrentPos()-1, 8 + (hasSize?1:0), stats->GetVal(wxT("tup_inserted")));
-			    	statistics->SetItem(stats->CurrentPos()-1, 9 + (hasSize?1:0), stats->GetVal(wxT("tup_updated")));
-			    	statistics->SetItem(stats->CurrentPos()-1, 10 + (hasSize?1:0), stats->GetVal(wxT("tup_deleted")));
+			    	statistics->SetItem(statistics->GetItemCount() - 1, 6 + (hasSize?1:0), stats->GetVal(wxT("tup_returned")));
+			    	statistics->SetItem(statistics->GetItemCount() - 1, 7 + (hasSize?1:0), stats->GetVal(wxT("tup_fetched")));
+			    	statistics->SetItem(statistics->GetItemCount() - 1, 8 + (hasSize?1:0), stats->GetVal(wxT("tup_inserted")));
+			    	statistics->SetItem(statistics->GetItemCount() - 1, 9 + (hasSize?1:0), stats->GetVal(wxT("tup_updated")));
+			    	statistics->SetItem(statistics->GetItemCount() - 1, 10 + (hasSize?1:0), stats->GetVal(wxT("tup_deleted")));
 			    }
             }
             stats->MoveNext();