aa5ca41bf512b00812f7874817932b4941511c4f
Dave Page <dpage@pgadmin.org>
2010-07-30 17:37:45 +0100

    Fix the GIT versioning script so it doesn't wipe out the value in svnversion.h if building from a tarball.

--- pgadmin/ver_svn.sh
+++ pgadmin/ver_svn.sh
@@ -5,6 +5,13 @@ if [ -d .svn -o -d ../.git ]; then
    if [ -f include/svnversion.h ]; then
       OV=$(cat include/svnversion.h | sed -e 's/#define VERSION_SVN "\(.*\)"/\1/')
    fi
+else
+   if [ ! -f include/svnversion.h ]; then
+       echo Creating svnversion.h with version: unknown
+       echo "#define VERSION_SVN \"unknown\"" > include/svnversion.h
+   fi
+
+   exit
 fi
 
 if [ -d .svn ]; then
@@ -15,6 +22,6 @@ fi
 
 
 if [ "$OV" != "$NV" ]; then
-   echo Refreshing svnversion.h
+   echo Refreshing svnversion.h with version: $NV
    echo "#define VERSION_SVN \"$NV\"" > include/svnversion.h
 fi
