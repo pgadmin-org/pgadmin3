91352c9d3831dc64f372ecbf8a330eb4021ebe06
Dave Page <dpage@pgadmin.org>
2011-01-25 15:35:56 +0000

    Store the servers on exit, to ensure we store the last database/schema etc.

--- pgadmin/frm/frmMain.cpp
+++ pgadmin/frm/frmMain.cpp
@@ -225,6 +225,9 @@ frmMain::frmMain(const wxString &title)
 
 frmMain::~frmMain()
 {
+	// Store the servers, to ensure we store the last database/schema etc
+	StoreServers();
+
 	settings->Write(wxT("frmMain/Perspective-") + wxString(FRMMAIN_PERSPECTIVE_VER), manager.SavePerspective());
 	manager.UnInit();
 
