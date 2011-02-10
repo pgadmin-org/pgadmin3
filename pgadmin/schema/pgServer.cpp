f7d7c721c525c832dca7873fbf3bf8ccda4fdefc
Guillaume Lelarge <guillaume@lelarge.info>
2011-02-10 23:09:50 +0100

    Add 3 more informations from pg_stat_replication
    Heikki added today three more informations in the pg_stat_replication stats
    view. They are added to the pseudo query displayed in the statistics tab of a
    server.

--- pgadmin/schema/pgServer.cpp
+++ pgadmin/schema/pgServer.cpp
@@ -1082,6 +1082,7 @@ void pgServer::ShowStatistics(frmMain *form, ctlListView *statistics)
 	if (conn)
 	{
 		wxString sql;
+        wxString replication_query = wxT("state || ' (' || sent_location || ' sent, ' || write_location || ' written, ' || flush_location || ' flushed, ' || apply_location || ' applied)'");
 		wxLogInfo(wxT("Displaying statistics for server %s"), GetIdentifier().c_str());
 
 		// Add the statistics view columns
@@ -1098,8 +1099,12 @@ void pgServer::ShowStatistics(frmMain *form, ctlListView *statistics)
 
 		sql = wxT("SELECT procpid, usename, datname, backend_start, client_addr, client_port, current_query FROM pg_stat_activity\n");
 		if (GetConnection()->BackendMinimumVersion(9, 1))
+        {
 			sql += wxT("UNION\n")
-			       wxT("SELECT procpid, usename, '' AS datname, backend_start, client_addr, client_port, state || ' (' || sent_location || ')' AS current_query FROM pg_stat_replication");
+			       wxT("SELECT procpid, usename, '' AS datname, backend_start, client_addr, client_port, ")
+                   + replication_query + wxT(" AS current_query FROM pg_stat_replication");
+        }
+
 		pgSet *stats = ExecuteSet(sql);
 		if (stats)
 		{
