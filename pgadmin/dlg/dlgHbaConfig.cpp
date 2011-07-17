693cd1d6cc573e40f202fa3bb6e145690d4a76be
Guillaume Lelarge <guillaume@lelarge.info>
2011-07-17 11:10:32 +0200

    Add new authentication, and database options

--- pgadmin/dlg/dlgHbaConfig.cpp
+++ pgadmin/dlg/dlgHbaConfig.cpp
@@ -75,15 +75,31 @@ dlgHbaConfig::dlgHbaConfig(pgFrame *parent, pgHbaConfigLine *_line, pgConn *_con
 
 	cbDatabase->Append(wxT("all"));
 	cbDatabase->Append(wxT("sameuser"));
-	cbDatabase->Append(wxT("samegroup"));
 	cbDatabase->Append(wxT("@<filename>"));
+	if (conn)
+	{
+		// role is supported from 8.1
+		if (conn->BackendMinimumVersion(8, 1))
+			cbDatabase->Append(wxT("samerole"));
+		else
+			cbDatabase->Append(wxT("samegroup"));
+
+		// replication is supported from 9.0
+		if (conn->BackendMinimumVersion(9, 0))
+			cbDatabase->Append(wxT("replication"));
+	}
+	else
+	{
+		cbDatabase->Append(wxT("samegroup"));
+		cbDatabase->Append(wxT("samerole"));
+		cbDatabase->Append(wxT("replication"));
+	}
 
 	cbUser->Append(wxT("all"));
 
 	cbMethod->Append(wxT("trust"));
 	cbMethod->Append(wxT("reject"));
 	cbMethod->Append(wxT("md5"));
-	cbMethod->Append(wxT("crypt"));
 	cbMethod->Append(wxT("password"));
 	cbMethod->Append(wxT("krb4"));
 	cbMethod->Append(wxT("krb5"));
@@ -104,9 +120,26 @@ dlgHbaConfig::dlgHbaConfig(pgFrame *parent, pgHbaConfigLine *_line, pgConn *_con
 		}
 
 		// CERT is supported from 8.4
+		// but crypt is no longer supported in 8.4
 		if (conn->BackendMinimumVersion(8, 4))
 		{
 			cbMethod->Append(wxT("cert"));
+		}
+		else
+		{
+			cbMethod->Append(wxT("crypt"));
+		}
+
+		// Radius is supported from 9.0
+		if (conn->BackendMinimumVersion(9, 0))
+		{
+			cbMethod->Append(wxT("radius"));
+		}
+
+		// Peer is supported from 9.1
+		if (conn->BackendMinimumVersion(9, 1))
+		{
+			cbMethod->Append(wxT("peer"));
 		}
 	}
 	else
@@ -116,6 +149,9 @@ dlgHbaConfig::dlgHbaConfig(pgFrame *parent, pgHbaConfigLine *_line, pgConn *_con
 		cbMethod->Append(wxT("gss"));
 		cbMethod->Append(wxT("sspi"));
 		cbMethod->Append(wxT("cert"));
+		cbMethod->Append(wxT("crypt"));
+		cbMethod->Append(wxT("radius"));
+		cbMethod->Append(wxT("peer"));
 	}
 
 	if (conn)
@@ -186,7 +222,8 @@ void dlgHbaConfig::OnAddDatabase(wxCommandEvent &ev)
 		return;
 
 	wxString newDatabase;
-	if (database == wxT("all") || database == wxT("sameuser") || database == wxT("samegroup") || database.Left(1) == wxT("@"))
+	if (database == wxT("all") || database == wxT("sameuser") || database == wxT("samegroup")
+	    || database == wxT("samerole") || database == wxT("replication") || database.Left(1) == wxT("@"))
 		database = wxEmptyString;
 
 	if (sel == 3)    // file
