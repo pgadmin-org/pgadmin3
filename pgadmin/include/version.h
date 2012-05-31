4b3ada9a26b718dcb76c42f8192eb1d91f9ee7c7
Dave Page <dpage@pgadmin.org>
2012-05-31 13:31:40 +0100

    Bump version for 1.14.3

--- pgadmin/include/version.h
+++ pgadmin/include/version.h
@@ -13,9 +13,9 @@
 #define VERSION_H
 
 // Application Versions
-#define VERSION_STR     wxT("1.14.2")
-#define VERSION_NUM     1,14,2,0
-#define VERSION_PACKAGE 1.14.2
+#define VERSION_STR     wxT("1.14.3")
+#define VERSION_NUM     1,14,3,0
+#define VERSION_PACKAGE 1.14.3
 
 #define PRERELEASE 0
 // #define BUILD "..."
