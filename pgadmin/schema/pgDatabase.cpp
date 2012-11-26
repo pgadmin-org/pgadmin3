808478e48f17c88228c6434ebc064d3ac54147e5
Magnus Hagander <magnus@hagander.net>
2012-11-26 17:30:56 +0100

    Fix incorrect JOIN in query to list databases
    The join to pg_shdescription needs to include the object class of the
    description, or it will return the same database multiple times for
    example when there is both a database and a user with the same oid,
    both having description. This can happen either when oid wraparound
    has happened in the server, or when pg_upgrade has been used (since
    pg_upgrade doesn't preserve database oid's, they can be reused in
    the same space as pg_authid)

--- pgadmin/schema/pgDatabase.cpp
+++ pgadmin/schema/pgDatabase.cpp
@@ -831,7 +831,7 @@ pgObject *pgDatabaseFactory::CreateObjects(pgCollection *collection, ctlTree *br
 		                datconnlimit + datcollate + datctype + seclabelsql +
 		                wxT("  FROM pg_database db\n")
 		                wxT("  LEFT OUTER JOIN pg_tablespace ta ON db.dattablespace=ta.OID\n")
-		                wxT("  LEFT OUTER JOIN pg_shdescription descr ON db.oid=descr.objoid\n")
+		                wxT("  LEFT OUTER JOIN pg_shdescription descr ON (db.oid=descr.objoid AND descr.objoid='pg_database'::regclass)\n")
 		                + restr +
 		                wxT(" ORDER BY datname"));
 	}
