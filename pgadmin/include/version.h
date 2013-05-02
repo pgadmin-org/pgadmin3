22b81f34aee40a90ed7fd755467cc2347455ab26
Dave Page <dpage@pgadmin.org>
2013-05-02 16:43:52 +0100

    Bump version for alpha release.

--- pgadmin/include/version.h
+++ pgadmin/include/version.h
@@ -13,9 +13,9 @@
 #define VERSION_H
 
 // Application Versions
-#define VERSION_STR     wxT("1.17.0 Dev")
-#define VERSION_NUM     1,17,0,0
-#define VERSION_PACKAGE 1.17.0-dev
+#define VERSION_STR     wxT("1.18.0 Alpha 1")
+#define VERSION_NUM     1,18,0,1
+#define VERSION_PACKAGE 1.18.0-alpha1
 
 #define PRERELEASE 1
 // #define BUILD "..."
