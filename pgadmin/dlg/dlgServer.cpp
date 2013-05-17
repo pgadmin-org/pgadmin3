a0f34f5e4bfd32128b363d9495d7f4b21187660f
Dave Page <dpage@pgadmin.org>
2013-05-17 10:11:54 +0100

    Fix SSL certificate authentication.

--- pgadmin/dlg/dlgServer.cpp
+++ pgadmin/dlg/dlgServer.cpp
@@ -591,6 +591,12 @@ pgObject *dlgServer::CreateObject(pgCollection *collection)
 	}
 
 	obj->iSetDbRestriction(txtDbRestriction->GetValue().Trim());
+	obj->iSetServiceID(txtServiceID->GetValue());
+	obj->SetSSLCert(pickerSSLCert->GetTextCtrlValue());
+	obj->SetSSLKey(pickerSSLKey->GetTextCtrlValue());
+	obj->SetSSLRootCert(pickerSSLRootCert->GetTextCtrlValue());
+	obj->SetSSLCrl(pickerSSLCrl->GetTextCtrlValue());
+	obj->iSetSSLCompression(chkSSLCompression->GetValue());
 
 	return obj;
 }
