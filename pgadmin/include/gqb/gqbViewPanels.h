b646554e22151ef3ceee26c2c6d6cf89cb6f6e3e
Dave Page <dpage@pgadmin.org>
2014-01-27 12:10:48 +0000

    Update copyright year.

--- pgadmin/include/gqb/gqbViewPanels.h
+++ pgadmin/include/gqb/gqbViewPanels.h
@@ -2,7 +2,7 @@
 //
 // pgAdmin III - PostgreSQL Tools
 //
-// Copyright (C) 2002 - 2013, The pgAdmin Development Team
+// Copyright (C) 2002 - 2014, The pgAdmin Development Team
 // This software is released under the PostgreSQL Licence
 //
 // gqbViewPanels.h - All panels used by GQB
