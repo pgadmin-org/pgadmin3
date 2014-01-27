71e567a841501a50efc14fd8776f81720bba704b
Dave Page <dpage@pgadmin.org>
2014-01-27 12:11:07 +0000

    Update copyright year.

--- pgadmin/include/hotdraw/tools/hdSelectAreaTool.h
+++ pgadmin/include/hotdraw/tools/hdSelectAreaTool.h
@@ -2,7 +2,7 @@
 //
 // pgAdmin III - PostgreSQL Tools
 //
-// Copyright (C) 2002 - 2013, The pgAdmin Development Team
+// Copyright (C) 2002 - 2014, The pgAdmin Development Team
 // This software is released under the PostgreSQL Licence
 //
 // hdSelectAreaTool.h - Tool to allow selection of figures inside a rectangle
