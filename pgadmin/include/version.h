48c63d136d6ccbbd6020f3d05b61edef74c39df9
Ashesh Vashi <ashesh.vashi@enterprisedb.com>
2015-08-03 18:08:27 +0530

    Bumped version for alpha-2 release (version: 1.22.0-alpha2)

--- pgadmin/include/version.h
+++ pgadmin/include/version.h
@@ -13,9 +13,9 @@
 #define VERSION_H
 
 // Application Versions
-#define VERSION_STR     wxT("1.22.0 Alpha 1")
-#define VERSION_NUM     1,22,0,1
-#define VERSION_PACKAGE 1.22.0-alpha1
+#define VERSION_STR     wxT("1.22.0 Alpha 2")
+#define VERSION_NUM     1,22,0,2
+#define VERSION_PACKAGE 1.22.0-alpha2
 
 #define PRERELEASE 0
 // #define BUILD "..."
